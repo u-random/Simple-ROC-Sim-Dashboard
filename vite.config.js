8f925bff768be78dbb7f7dd4b49d8abe94397912
u-random <isaklars@outlook.com>
2025-01-20 15:07:05 +0100

    Updated configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Vite build configuration (Default)
+// Vite build configuration (Edited)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    open: true // Automatically open browser
+  }
 })
