452677338acb41c7251bf82697738ddf8c86bc52
u-random <isaklars@outlook.com>
2025-02-01 11:40:37 +0100

    WiP. Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-//import { useState } from 'react'
 import './styles/App.css'
 import FleetOverview from './pages/FleetOverview';
+
+// This is the main window/collection of views
+// TODO: Add toggle between views
+
 
 function App() {
     return (
