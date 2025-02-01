490f0da3133db11b03b8f076d01efaa300d41a8b
u-random <isaklars@outlook.com>
2025-02-01 11:59:21 +0100

    Cleanup

---
+++ src/views/Map.jsx
@@ -0,0 +1,19 @@
+//import ImageDisplay from './DisplayImage';
+//import myImage from '../assets/Debugempty.png';
+
+const Map = () => {
+    return (
+        <div className="container-50">
+            <div className="map-text">
+                OSLOFJORD - 4km x 4km
+                <br/>
+                Center: XX° N, XX° E
+            </div>
+
+            <div className="placeholder-text">
+                MAP
+            </div>
+        </div>
+    );
+};
+export default Map;
