60027693b7f64c607030db9283348088b3e6dc64
u-random <isaklars@outlook.com>
2025-02-20 21:56:24 +0100

    Updates to main App file to support the dual main pages, and toggle between them. Also introduced context management at this level for the map in addition to the ship context.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
+import { useState } from 'react';
 import './styles/App.css'
 import FleetOverview from './pages/FleetOverview';
+import ShipOverview from './pages/ShipOverview';
 import { ShipProvider } from './components/ShipContext';
+import { MapProvider } from './components/MapContext';
 
 // This is the main window/collection of views
-// TODO: Add toggle between views
+// TODO: OK - Add toggle between views
+// TODO: Disallow view switch when in control mode
+// TODO: In regard to the above, have an alert to say "Going to Fleet Overview will exit Control Mode. Are you sure?"
+
 
 
 function App() {
+    const [currentView, setCurrentView] = useState('fleet'); // 'fleet' or 'ship'
+    const [isControlMode, setIsControlMode] = useState(false);
+
+
     return (
         <ShipProvider>
-            <div className="app-container">
-                <FleetOverview />
-            </div>
+            <MapProvider>
+                <div className="app-container">
+                    <header className="header">
+                        <div className="title">Remote Operation Center</div>
+                        <button
+                            onClick={() => setCurrentView('fleet')}
+                            className={currentView === 'fleet' ? 'active' : ''}
+                        >
+                            Fleet View
+                        </button>
+                        <button
+                            onClick={() => setCurrentView('ship')}
+                            className={currentView === 'ship' ? 'active' : ''}
+                        >
+                            Ship View
+                        </button>
+                        {isControlMode && <div className="control-mode-indicator">CONTROL MODE</div>}
+                    </header>
+
+                    {currentView === 'fleet' ? (
+                        <FleetOverview />
+                    ) : (
+                        <ShipOverview
+                            isControlMode={isControlMode}
+                            setIsControlMode={setIsControlMode}
+                        />
+                    )}
+                </div>
+            </MapProvider>
         </ShipProvider>
     );
 }
